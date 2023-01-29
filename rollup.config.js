34bf7badb37e6c4f30475d6bc80bc3455de45e16
G <370976188@qq.com>
2023-01-29 17:03:18 +0800

    首次提交

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'lib/index.esm.js',
+      format: 'es',
+    },
+    {
+      file: 'lib/index.umd.js',
+      format: 'umd',
+      name: 'GProgress',
+    },
+  ],
+  plugins: [
+    terser(),
+    commonjs(),
+    typescript()
+  ]
+};
